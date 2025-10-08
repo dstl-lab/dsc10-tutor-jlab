f4af335ed1c27ecbc01f19a98a20e21f95b19c36
Sam Lau <samlau95@gmail.com>
2025-10-08 12:14:56 -0400

    add example notebooks

---
+++ example_notebooks/lab2/tests/q4_4.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q4_4',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> type(most_improved) == str\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': ">>> most_improved[5] == ' '\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> most_improved[1] == 'x'\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}

8af9e290912564400c104d7e6e16e8be18de3abe
EllaLi86 <ell024@ucsd.edu>
2026-01-30 16:29:47 -0800

    update lab0

--- example_notebooks/lab2/tests/q2_1_2.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q2_1_2',
-    'points': None,
-    'suites': [{'cases': [{'code': '>>> twenty_percent == 20.0\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}

d4adf1e609b4cb4f0d2fcf500193cc739407c82f
EllaLi86 <ell024@ucsd.edu>
2026-02-03 15:46:29 -0800

    add prettier

--- src/api/file_tools_calls.ts
+++ src/api/file_tools_calls.ts
@@ -1,31 +1,30 @@
 import { requestAPI } from './index';
 export interface IReadFileParams {
-    file_path: string;
-    notebook_path?: string;
-  }
-  
-  export interface IReadFileResponse {
-    file_path: string;
-    resolved_path: string;
-    content: string;
-    truncated: boolean;
-    file_size: number;
-    content_length: number;
-  }
-  
-  export async function readFile({
-    file_path,
-    notebook_path
-  }: IReadFileParams): Promise<IReadFileResponse> {
-    return requestAPI<IReadFileResponse>('read-file', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        file_path,
-        notebook_path
-      })
-    });
-  }
-  
+  file_path: string;
+  notebook_path?: string;
+}
+
+export interface IReadFileResponse {
+  file_path: string;
+  resolved_path: string;
+  content: string;
+  truncated: boolean;
+  file_size: number;
+  content_length: number;
+}
+
+export async function readFile({
+  file_path,
+  notebook_path
+}: IReadFileParams): Promise<IReadFileResponse> {
+  return requestAPI<IReadFileResponse>('read-file', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      file_path,
+      notebook_path
+    })
+  });
+}
